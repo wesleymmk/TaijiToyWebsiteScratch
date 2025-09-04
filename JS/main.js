7a01f4ef698e156de3fd43878ca4ecc14003d919
wesleymmk <wesleymmk@gmail.com>
2025-09-04 17:30:41 -0500

    adding basic parameters to main.js

---
+++ JS/main.js
@@ -0,0 +1,26 @@
+
+// Find the main container element in the HTML
+const appContainer = document.getElementById('app');
+
+// --- Function to clear the current view ---
+function clearAppContainer() {
+    appContainer.innerHTML = '';
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// THIS HAS TO BE THE VERY BOTTOM
+// --- Initial Page Load ---
+// When the website first loads, show the login view.
+renderWelcomeView();
+// THIS HAS TO BE THE VERY BOTTOM
