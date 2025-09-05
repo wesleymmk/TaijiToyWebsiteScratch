63a89547e2c6f73465f50baeaae8bbccfed14c1b
tonyguzmann <161231403+tonyguzmann@users.noreply.github.com>
2025-09-05 10:54:49 -0500

    Add files via upload

---
+++ API/push_traits.php
@@ -0,0 +1,27 @@
+<?php
+header('Content-Type: application/json');
+
+$input = json_decode(file_get_contents('php://input'), true);
+if (!isset($input['coreValues'])) {
+    echo json_encode(['success' => false, 'message' => 'Missing coreValues']);
+    exit;
+}
+
+$ch = curl_init('http://localhost:3000/generate');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode(['coreValues' => $input['coreValues']]));
+
+$response = curl_exec($ch);
+$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+if (curl_errno($ch)) {
+    echo json_encode(['success' => false, 'message' => curl_error($ch)]);
+    curl_close($ch);
+    exit;
+}
+
+curl_close($ch);
+http_response_code($httpCode);
+echo $response;
