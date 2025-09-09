55db18bb30a20a2ea20a2ca1bf6790b0a5c780b6
wesleymmk <wesleymmk@gmail.com>
2025-09-09 17:33:58 -0500

    Adding clearAppContainer function to Common_Functions.js along with comments describing the file.

---
+++ JS/Common_Function.js
@@ -0,0 +1,8 @@
+// This file contains common functions used in multiple .js files.
+// Its primary purpose is to reduce the code base's size and increase organization.
+// It does this through having 'common functions' such as the clear HTML code function.
+
+//This is the HTML Page Clear function. 
+export function clearAppContainer() {  // WM code // from online modified it so that it can be exported and used accross the server
+    appContainer.innerHTML = '';
+}
