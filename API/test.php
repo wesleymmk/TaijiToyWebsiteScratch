2f9e38e03e38586c5349e6a657275c0d13289541
wesleymmk <wesleymmk@gmail.com>
2025-09-23 15:02:12 -0500

    update to check loop inside of test.php for making sure the output was properly created.

--- API/test.php
+++ API/test.php
@@ -1,8 +1,8 @@
-
-// test function for saving entire orders to 
 <?php
 session_start();
 require_once 'config.php';
+
+// test function for saving entire orders to 
 
 
 
@@ -37,22 +37,23 @@ header('Content-Type: application/json');
 	// define area for toy 6
 	INSERT INTO output_details (id, order_id, color_1, color_2, attribute_1, attribute_2, desc_short, desc_long, image_path) VALUES (/*assign order ID */? , /*color ID 1*/? , /*color ID 2*/? , /*attribute 1*/? , /*attribute 2 */? , /*short description */? , /*long description */?);
 
-
-
 	// check function for checking that output saved porperly
 	if (!is_array($generated_products) || count($generated_products) !== 6) 
 	{
     throw new Exception("error in num of arrays retrieved");
 	}
-
-	$first_product = $generated_products[0];
-	if (
-		!isset($first_product['color_1']) || 
-		!isset($first_product['color_2']) || 
-		!isset($first_product['attribute_1']) || 
-		!isset($first_product['attribute_2']) || 
-		!isset($first_product['desc_short']) || 
-		!isset($first_product['desc_long'])
-	) {
-		throw new Exception("Error in product data");
+	
+	// checks each array in generated products  
+	for($i = 0, $i < 6, $i++)
+	{
+		if (
+			!isset($generated_products[i]['color_1']) || 
+			!isset($generated_products[i]['color_2']) || 
+			!isset($generated_products[i]['attribute_1']) || 
+			!isset($generated_products[i]['attribute_2']) || 
+			!isset($generated_products[i]['desc_short']) || 
+			!isset($generated_products[i]['desc_long'])
+		) {
+			throw new Exception("Error in product data line ' . $i . '.");
+		}
 	}
