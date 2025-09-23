ea8e3c4e310fe949903b134fa6ed22b2767fcb12
wesleymmk <wesleymmk@gmail.com>
2025-09-23 14:13:50 -0500

    Cleaning Code base for main.js

---
+++ js/main.js
@@ -0,0 +1,38 @@
+//*************************************************************************************************************************//
+//***********************BRAND NEW IMPLEMENTATION OF WEBSITE TESTING CURRENTLY - PS****************************************//
+//*************************************************************************************************************************//
+
+import * as WelcomePage from './Authentication_Page.js';
+import * as Account from './User_Account.js';
+import * as InputOrder from './Order_Generation.js';
+import * as OutputOrder from './Order_Gen_Output.js';
+import * as ComUtils from './Common_Function.js';
+
+const contentContainer = document.getElementById('content');
+
+function loadPage() {
+    const hash = window.location.hash.substring(1); // Get the hash, e.g., 'home', 'about', 'contact'
+
+    let content = '';
+    switch (hash) {
+        case 'order-output':
+            content = OutputOrder.renderGenerationOutputView();
+            break;
+        case 'order-input':
+            content = InputOrder.renderGenerationInputView();
+            break;
+        case 'account':
+            content = Account.renderUserAccount();
+            break;
+        case 'welcome-page':
+        default:
+            content = WelcomePage.renderWelcomeView();
+            break;
+    }
+
+    contentContainer.innerHTML = content;
+}
+
+window.addEventListener('hashchange', loadPage);
+
+loadPage();
