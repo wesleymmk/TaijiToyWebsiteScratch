997cef7ce3f6770fce37cdedb0baab854b261f6a
wesleymmk <wesleymmk@gmail.com>
2025-09-25 15:22:31 -0500

    updates to test.php and introductions of common functions.php

---
+++ API/common_function.php
@@ -0,0 +1,11 @@
+<?php
+
+// =================================================================
+// COMMON FUNCTIONS LIBRARY
+// =================================================================
+// This file is a "toolbox" for your other PHP scripts.
+// It should only contain all reusable function definitions used throughout the backend.
+// =================================================================
+
+
+?>
