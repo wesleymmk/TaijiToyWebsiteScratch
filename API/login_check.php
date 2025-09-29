9ad83f35524cd8d6fb9ab286419b37881173059e
wesleymmk <wesleymmk@gmail.com>
2025-09-29 15:04:45 -0500

    login_check.php created

--- API/login_check.php
+++ API/login_check.php
@@ -1,11 +1,30 @@
 <?php
 session_start();
-require_once 'config.php';
-require_once 'common_functions.php';
 header('Content-Type: application/json');
+// =================================================================
+// Log in Check Function
+// =================================================================
+// This file is a log in check function callable by the front end to ensure the user accessing the page is logged in
+// Created by WAM on 9/29/25
+//
+// =================================================================
 
 
+// Check if the 'customer_id' is set in the session.
+if (isset($_SESSION['customer_id'])) 
+{
+    echo json_encode
+    ([
+        'loggedin' => true
+    ]);
+} 
+else {
+    echo json_encode
+    ([
+        'loggedin' => false
+    ]);
+}
 
-
-
+// ComUtils.apiCall(login_check.php, {})
+// 
 ?>
