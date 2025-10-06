43fe3561c6ab8c91563d76879f8acaeac8a16299
wesleymmk <wesleymmk@gmail.com>
2025-10-06 15:55:17 -0500

    gitignore update/ display traits function definition

---
+++ API/display_traits.php
@@ -0,0 +1,13 @@
+<?php
+// Made by WAM 10/6/25 
+// display_traits.php — Receives unique order ID and accesses SQL to get all the traits
+// === Load dependencies ===
+require_once 'config.php';
+require_once 'common_function.php';
+session_start();
+
+
+
+
+
+?>
