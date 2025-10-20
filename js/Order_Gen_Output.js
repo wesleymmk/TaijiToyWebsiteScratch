4459f054fa4603d3299455c39b217c5d1f9bed4b
wesleymmk <wesleymmk@gmail.com>
2025-10-20 09:27:54 -0500

    adding parsing logic to Order_Gen_Output

--- js/Order_Gen_Output.js
+++ js/Order_Gen_Output.js
@@ -7,14 +7,79 @@ import * as AccUtils from './User_Account.js';
 
 export function renderGenerationOutputView() {
     ComUtils.clearAppContainer();
+
+    // TEST DATA
+    ///*
+    const response = {
+        "success": true,
+        "message": "Successfully retrieved details for order ID 10",
+        "data": [
+            { "id": 19, "order_id": 10, "color_1": "Blue", "color_2": "Red", "attribute_1": "Stability", "attribute_2": "Adventure", "desc_short": "Short desc 1...", "desc_long": "Long desc 1...", "image_path": null },
+            { "id": 20, "order_id": 10, "color_1": "Grey", "color_2": "Yellow", "attribute_1": "Tradition", "attribute_2": "Innovation", "desc_short": "Short desc 2...", "desc_long": "Long desc 2...", "image_path": null },
+            { "id": 21, "order_id": 10, "color_1": "Brown", "color_2": "Pink", "attribute_1": "Groundedness", "attribute_2": "Playfulness", "desc_short": "Short desc 3...", "desc_long": "Long desc 3...", "image_path": null },
+            { "id": 22, "order_id": 10, "color_1": "White", "color_2": "Black", "attribute_1": "Innocence", "attribute_2": "Experience", "desc_short": "Short desc 4...", "desc_long": "Long desc 4...", "image_path": null },
+            { "id": 23, "order_id": 10, "color_1": "Green", "color_2": "Orange", "attribute_1": "Growth", "attribute_2": "Vitality", "desc_short": "Short desc 5...", "desc_long": "Long desc 5...", "image_path": null },
+            { "id": 24, "order_id": 10, "color_1": "Silver", "color_2": "Gold", "attribute_1": "Reflection", "attribute_2": "Action", "desc_short": "Short desc 6...", "desc_long": "Long desc 6...", "image_path": null }
+        ]
+    };
+    //*/
+    //WM added parsing for taking toy array appart into seprate elements
+    const allToysArray = response.data;
+ 
+    const toy1 = allToysArray[0];
+    const toy2 = allToysArray[1];
+    const toy3 = allToysArray[2];
+    const toy4 = allToysArray[3];
+    const toy5 = allToysArray[4];
+    const toy6 = allToysArray[5];
+
+    const output_color_1 = toy1.color_1;
+    const output_color_2 = toy1.color_2;
+    const output_color_3 = toy2.color_1;
+    const output_color_4 = toy2.color_2;
+    const output_color_5 = toy3.color_1;
+    const output_color_6 = toy3.color_2;
+    const output_color_7 = toy4.color_1;
+    const output_color_8 = toy4.color_2;
+    const output_color_9 = toy5.color_1;
+    const output_color_10 = toy5.color_2;
+    const output_color_11 = toy6.color_1;
+    const output_color_12 = toy6.color_2;
+    const output_attribute_1 = toy1.attribute_1;
+    const output_attribute_2 = toy1.attribute_2;
+    const output_attribute_3 = toy2.attribute_1;
+    const output_attribute_4 = toy2.attribute_2;
+    const output_attribute_5 = toy3.attribute_1;
+    const output_attribute_6 = toy3.attribute_2;
+    const output_attribute_7 = toy4.attribute_1;
+    const output_attribute_8 = toy4.attribute_2;
+    const output_attribute_9 = toy5.attribute_1;
+    const output_attribute_10 = toy5.attribute_2;
+    const output_attribute_11 = toy6.attribute_1;
+    const output_attribute_12 = toy6.attribute_2;
+    const output_desc_short_toy1 = toy1.desc_short;
+    const output_desc_long_toy1 = toy1.desc_long;
+    const output_desc_short_toy2 = toy2.desc_short;
+    const output_desc_long_toy2 = toy2.desc_long;
+    const output_desc_short_toy3 = toy3.desc_short;
+    const output_desc_long_toy3 = toy3.desc_long;
+    const output_desc_short_toy4 = toy4.desc_short;
+    const output_desc_long_toy4 = toy4.desc_long;
+    const output_desc_short_toy5 = toy5.desc_short;
+    const output_desc_long_toy5 = toy5.desc_long;
+    const output_desc_short_toy6 = toy6.desc_short;
+    const output_desc_long_toy6 = toy6.desc_long;
+ 
+
     //PS added navwrapper, This is the new implementation of the navbar. Removed html version.
     const navwrapper = document.createElement('div');
-    const Trait1 = 'Placeholder 1';
-    const Trait2 = 'Placeholder 2';
-    const Trait3 = 'Placeholder 3';
-    const Trait4 = 'Placeholder 4';
-    const Trait5 = 'Placeholder 5';
-    const Trait6 = 'Placeholder 6';
+    const Trait1 = output_attribute_1;
+    const Trait2 = output_attribute_2;
+    const Trait3 = output_attribute_3;
+    const Trait4 = output_attribute_4;
+    const Trait5 = output_attribute_5;
+    const Trait6 = output_attribute_6;
+
     navwrapper.id = 'order-output';
     navwrapper.classList.add('nav-wrapper');
     navwrapper.appendChild(ComUtils.HomeLogo);
