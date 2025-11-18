578ddd2d567fecf82c5f9242d9d9daea9ba9183c
wesleymmk <wesleymmk@gmail.com>
2025-11-18 16:30:01 -0600

    Prompt input replaced. (deleted by github when github was having issues)

--- js/Prompt_Input.js
+++ js/Prompt_Input.js
@@ -1,2 +1,42 @@
-429: Too Many Requests
-For more on scraping GitHub and how it may affect your rights, please review our Terms of Service (https://docs.github.com/en/site-policy/github-terms/github-terms-of-service).
+export function renderGenerationInputView() {
+    ComUtils.clearAppContainer();
+    //ComUtils.header();
+
+    let heading = document.createElement('h1');
+    heading.classList.add('header');
+    heading.textContent = 'Generation Input Page';
+
+    let GiftOption = document.createElement('input');
+    GiftOption.type = 'checkbox';
+    GiftOption.name = 'giftOption';
+
+    let CheckboxLabel = document.createElement('label');
+    CheckboxLabel.htmlFor = 'GiftOption';
+    CheckboxLabel.textContent = 'Is This a Gift?';
+
+    let CustomerInput = document.createElement('input');
+    CustomerInput.type = 'text';
+    CustomerInput.placeholder = 'Enter your Values';
+    CustomerInput.required = true;
+
+    let SubmitGeneration = document.createElement("button");
+    SubmitGeneration.textContent = 'Submit';
+    SubmitGeneration.classList.add('LoginButton');
+    SubmitGeneration.type = "submit";
+
+    /*registerForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        const text = CustomerInput.value;
+
+        // Backend code needed.
+        
+    });*/
+
+    appContainer.appendChild(heading);
+    appContainer.appendChild(GiftOption);
+    appContainer.appendChild(CheckboxLabel);
+    appContainer.appendChild(CustomerInput);
+    appContainer.appendChild(SubmitGeneration);
+
+}
